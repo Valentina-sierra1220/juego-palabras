2e7370ae52cfa116ade9fc7fcce59e0fb5782d49
valentina sierra <valentina.sierra.ospina1220thc@gmail.com>
2025-04-05 20:45:35 -0500

    Inicializar vidas, puntos, racha, dificultad y logros

--- juego-poo/logica_palabras.py
+++ juego-poo/logica_palabras.py
@@ -8,3 +8,9 @@ class JuegoPalabrasDificil:
     "frutas": ["mandarina", "frambuesa", "cereza", "arándano", "ciruela", "pomelo"],
     "paises": ["azerbaiyan", "mozambique", "singapur", "ucrania", "argentina", "colombia"]
 }
+        self.vidas = 5
+        self.puntos = 0
+        self.racha = 0
+        self.dificultad = 1
+        self.palabras_usadas = set()
+        self.logros = set()
