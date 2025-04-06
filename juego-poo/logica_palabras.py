c14871358d9c3d5cae785a3fcedbbd297e04b2ef
valentina sierra <valentina.sierra.ospina1220thc@gmail.com>
2025-04-05 20:43:42 -0500

    Agregar diccionario de categorías al constructor

--- juego-poo/logica_palabras.py
+++ juego-poo/logica_palabras.py
@@ -3,5 +3,8 @@ import time
 
 class JuegoPalabrasDificil:
     def _init_(self):
-        pass
-    
+        self.categorias = {
+    "animales": ["hipopotamo", "murcielago", "camaleon", "rinoceronte", "alce", "jaguar"],
+    "frutas": ["mandarina", "frambuesa", "cereza", "arándano", "ciruela", "pomelo"],
+    "paises": ["azerbaiyan", "mozambique", "singapur", "ucrania", "argentina", "colombia"]
+}
